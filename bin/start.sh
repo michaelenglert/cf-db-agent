2f1eb6d77d8cd777a3598303399c57d71ccac173
michaelenglert <michi.eng@gmail.com>
2016-10-11 17:42:30 +0200

    Added Unique Host ID

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,4 @@
 $PWD/.java-buildpack/open_jdk_jre/bin/java $JAVA_OPTS  \
 -Ddbagent.name="$(ruby -e "require 'json' ; a = JSON.parse(ENV['VCAP_APPLICATION']);puts \"#{a['application_name']}\"")" \
+-Dappdynamics.agent.uniqueHostId="$(ruby -e "require 'json' ; a = JSON.parse(ENV['VCAP_APPLICATION']);puts \"#{a['application_name']}:#{a['instance_index']}\"")" \
 -jar $PWD/db-agent.jar
